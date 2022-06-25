8bdf9a0a1b955da64b7b5ce5ae6e386e1913254b
FlyingMedusa <sleboda.m98@gmail.com>
2022-06-25 22:56:12 +0200

    node notes week 4 - cryptography project

---
+++ projectEncryptDecrypt/encrypt.js
@@ -0,0 +1,14 @@
+const { readFile, writeFile } = require('fs').promises;
+const { encryptText, hash } = require('./cipher');
+const { ENCRYPTED_SALT, HASH_SALT } = require('./constants');
+
+const [,, fileName, pwd] = process.argv;
+
+(async () => {
+  const content = await readFile(fileName, 'utf8');
+  const contentHash = hash(content, HASH_SALT);
+  const encrypted = await encryptText(content, pwd, ENCRYPTED_SALT);
+  encrypted.hash = contentHash;
+  await writeFile(fileName, JSON.stringify(encrypted), 'utf8');
+  console.log('Done.');
+})();
